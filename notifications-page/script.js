d643ff063598f8554ee6ddabdd6b8af389334432
Sarah Cochrane <sarah.kristine.cochrane@gmail.com>
2022-10-23 00:10:19 -0700

    notifications page challenge - css update

---
+++ notifications-page/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// 1) click Mark As Read button ...
+//      1A) .dot ADD "hidden"
+//      1B) .notification REMOVE "unread"
+//      1C) .notificationNumber text = 0
+
+// 2) click any notification w/ class "unread"...
+//      2A) .dot ADD "hidden"
+//      2B) .notification REMOVE "unread"
+//      2C) .notificationNumber text = number of .notifications with class "unread"
