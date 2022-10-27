39c64e16a376d0ad266e330645c61dac849d7b56
Sarah Cochrane <sarah.kristine.cochrane@gmail.com>
2022-10-26 20:43:04 -0700

    final notifications page

--- notifications-page/script.js
+++ notifications-page/script.js
@@ -9,3 +9,18 @@
 //      2A) .dot ADD "hidden"
 //      2B) .notification REMOVE "unread"
 //      2C) .notificationNumber text = number of .notifications with class "unread"
+
+let number = document.querySelector(".notification-number");
+const unreadDots = document.querySelectorAll(".dot");
+const notifications = document.querySelectorAll(".notification");
+const markAsRead = document.querySelector(".mark-read");
+
+markAsRead.addEventListener("click", function () {
+  for (const notification of notifications) {
+    notification.classList.remove("unread");
+    number.textContent = document.querySelectorAll(".unread").length;
+    for (const unreadDot of unreadDots) {
+      unreadDot.classList.add("hidden");
+    }
+  }
+});
